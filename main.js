3516ef4d47e218bcb5801b2b42eb562b91fb02f2
Mariusz <mariusz.c008@protonmail.com>
2023-08-17 16:21:02 +0200

    homework4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('wtf!');
+// alert('wtf!');
 const firstName='Mariusz';
 const age=35;
 
@@ -6,3 +6,17 @@ console.log(firstName);
 console.log(age);
 
 console.log(`Cześć! Mam na imię ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('.page-header__heading--js')
+
+//console.log(header)
+
+//header.innerHTML=`Cześć! Mam na imię ${firstName} i mam ${age} lat`
+
+//console.log(header.style)
+
+//header.style.color='red';
+
+const contentJs=document.querySelector('.my-article__content--js')
+
+contentJs.innerHTML='Jestem wypelniony Javascriptem'
