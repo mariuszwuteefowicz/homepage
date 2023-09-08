33a2f5c41d15783f96717ee866a5db089561ec9d
Mariusz <mariusz.c008@protonmail.com>
2023-09-08 12:01:46 +0200

    npm init

---
+++ skrypt.js
@@ -0,0 +1,10 @@
+const mojaZmienna ='hello';
+console.log(`${mojaZmienna} Mariusz`);
+console.log('siema');
+
+const firstName='Mariusz';
+const age=35;
+
+console.log(firstName);
+console.log(age);
+console.log(`Cześć! Mam na imię ${mojaZmienna} i mam ${age} lat`);
